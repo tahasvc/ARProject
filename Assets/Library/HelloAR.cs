3b1a8ab70c48aa9dcb1fb4491af10cda8b214a11
kkazdal <kkazdal@users.noreply.github.com>
2019-12-24 20:52:20 +0300

    gameobject kodum gencler
    Signed-off-by: kkazdal <kkazdal@users.noreply.github.com>

--- Assets/Library/HelloAR.cs
+++ Assets/Library/HelloAR.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class HelloAR : MonoBehaviour
 {
+    [SerializeField]
+    int Hiz = 10;
+
+
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello AR Projectsssss :)");
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float yatay = Input.GetAxis("Horizontal");
+
+        float dikey = Input.GetAxis("Vertical");
+        Vector3 hareket = new Vector3(yatay * Hiz, 0.0f, dikey * Hiz);
+        rb.AddForce(hareket);//force güç ekle demek
     }
 }
